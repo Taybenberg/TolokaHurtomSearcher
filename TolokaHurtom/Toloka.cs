f82b0d654316d5cc84a8239bf278b2b7043e8ab6
Вдовенко Віталій <taybenberg@gmail.com>
2018-11-27 00:18:28 +0200

    Cleaning the code

--- TolokaHurtom/Toloka.cs
+++ TolokaHurtom/Toloka.cs
@@ -1,114 +1,111 @@
-﻿using Newtonsoft.Json;
-using System.Net;
+﻿using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 
-namespace TolokaHurtom
+public class Toloka
 {
-    public class Toloka
+    private const string TolokaUrl = "https://toloka.to/api.php";
+    private const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.17763";
+
+    private Hurtom[] result;
+
+    public class Hurtom
     {
-        private const string TolokaUrl = "https://toloka.to/api.php";
-        private const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.17763";
+        public string id { get; set; }
+        public string link { get; set; }
+        public string title { get; set; }
+        public string forum_name { get; set; }
+        public string forum_parent { get; set; }
+        public string comments { get; set; }
+        public string size { get; set; }
+        public string seeders { get; set; }
+        public string leechers { get; set; }
+        public string complete { get; set; }
 
-        private Hurtom[] result;
-
-        public class Hurtom
+        public override string ToString()
         {
-            public string id { get; set; }
-            public string link { get; set; }
-            public string title { get; set; }
-            public string forum_name { get; set; }
-            public string forum_parent { get; set; }
-            public string comments { get; set; }
-            public string size { get; set; }
-            public string seeders { get; set; }
-            public string leechers { get; set; }
-            public string complete { get; set; }
-
-            public override string ToString()
-            {
-                return
-                    $"Id: {id}" +
-                    $"\nПосилання: {link}" +
-                    $"\nНазва: {title}" +
-                    $"\nФорум: {forum_name}" +
-                    $"\nТолока: {forum_parent}" +
-                    $"\nКоментарів: {comments}" +
-                    $"\nРозмір: {size}" +
-                    $"\nРоздають: {seeders}" +
-                    $"\nЗавантажують: {leechers}" +
-                    $"\nЗавантажень: {complete}";
-            }
+            return
+                $"Id: {id}" +
+                $"\nПосилання: {link}" +
+                $"\nНазва: {title}" +
+                $"\nФорум: {forum_name}" +
+                $"\nТолока: {forum_parent}" +
+                $"\nКоментарів: {comments}" +
+                $"\nРозмір: {size}" +
+                $"\nРоздають: {seeders}" +
+                $"\nЗавантажують: {leechers}" +
+                $"\nЗавантажень: {complete}";
         }
+    }
 
-        public Toloka(string search1)
+    public Toloka(string search1)
+    {
+        using (var webClient = new WebClient())
         {
-            using (var webClient = new WebClient())
-            {
-                webClient.Headers.Add("user-agent", UserAgent);
-                webClient.QueryString.Add("search", Regex.Escape(search1));
-                webClient.UseDefaultCredentials = true;
+            webClient.Headers.Add("user-agent", UserAgent);
+            webClient.QueryString.Add("search", Regex.Escape(search1));
+            webClient.UseDefaultCredentials = true;
 
-                fillArray(webClient.DownloadString(TolokaUrl));
-            }
+            fillArray(webClient.DownloadString(TolokaUrl));
         }
+    }
 
-        public Toloka(string search1, string search2)
+    public Toloka(string search1, string search2)
+    {
+        using (var webClient = new WebClient())
         {
-            using (var webClient = new WebClient())
-            {
-                webClient.Headers.Add("user-agent", UserAgent);
-                webClient.QueryString.Add("search", Regex.Escape(search1));
-                webClient.QueryString.Add("search2", Regex.Escape(search2));
-                webClient.UseDefaultCredentials = true;
+            webClient.Headers.Add("user-agent", UserAgent);
+            webClient.QueryString.Add("search", Regex.Escape(search1));
+            webClient.QueryString.Add("search2", Regex.Escape(search2));
+            webClient.UseDefaultCredentials = true;
 
-                fillArray(webClient.DownloadString(TolokaUrl));
-            }
+            fillArray(webClient.DownloadString(TolokaUrl));
         }
+    }
 
-        public Toloka(string search1, string search2, string search3)
+    public Toloka(string search1, string search2, string search3)
+    {
+        using (var webClient = new WebClient())
         {
-            using (var webClient = new WebClient())
-            {
-                webClient.Headers.Add("user-agent", UserAgent);
-                webClient.QueryString.Add("search", Regex.Escape(search1));
-                webClient.QueryString.Add("search2", Regex.Escape(search2));
-                webClient.QueryString.Add("search3", Regex.Escape(search3));
-                webClient.UseDefaultCredentials = true;
+            webClient.Headers.Add("user-agent", UserAgent);
+            webClient.QueryString.Add("search", Regex.Escape(search1));
+            webClient.QueryString.Add("search2", Regex.Escape(search2));
+            webClient.QueryString.Add("search3", Regex.Escape(search3));
+            webClient.UseDefaultCredentials = true;
 
-                fillArray(webClient.DownloadString(TolokaUrl));
-            }
+            fillArray(webClient.DownloadString(TolokaUrl));
         }
+    }
 
-        private void fillArray(string query)
-        {
-            string jsonString = WebUtility.HtmlDecode(Regex.Unescape(query));
-            result = JsonConvert.DeserializeObject<Hurtom[]>(jsonString);
-        }
+    private void fillArray(string query)
+    {
+        string jsonString = WebUtility.HtmlDecode(Regex.Unescape(query));
+        result = JsonConvert.DeserializeObject<Hurtom[]>(jsonString);
+    }
 
-        public Hurtom[] ToArray()
-        {
-            return result;
-        }
+    public Hurtom[] ToArray()
+    {
+        return result;
+    }
 
-        public string ToString(string separator = null)
-        {
-            StringBuilder res = new StringBuilder();
+    public string ToString(string separator = null)
+    {
+        StringBuilder res = new StringBuilder();
 
-            foreach (var r in result)
-                res.Append($"{r}{separator}");
+        foreach (var r in result)
+            res.Append($"{r}{separator}");
 
-            return res.ToString();
-        }
+        return res.ToString();
+    }
 
-        public string[] ToStringArray()
-        {
-            string[] res = new string[result.Length];
+    public string[] ToStringArray()
+    {
+        string[] res = new string[result.Length];
 
-            for (int i = 0; i < result.Length; i++)
-                res[i] = result[i].ToString();
+        for (int i = 0; i < result.Length; i++)
+            res[i] = result[i].ToString();
 
-            return res;
-        }
+        return res;
     }
 }
