9340ce39e938ab81b5d5452005b95964063727a0
Вдовенко Віталій <taybenberg@gmail.com>
2019-10-11 20:09:14 +0300

    HurtomBot

---
+++ HurtomBot/HurtomBot.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InputFiles;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.InlineQueryResults;
+
+namespace HurtomBot
+{
+    public class HurtomBot
+    {
+        readonly string TelegramApiToken;
+
+        public HurtomBot(string TelegramApiToken)
+        {
+            var Bot = new TelegramBotClient(TelegramApiToken);
+
+            Bot.SetWebhookAsync("");
+
+            Bot.OnInlineQuery += async (object updobj, InlineQueryEventArgs iqea) =>
+            {
+                if (string.IsNullOrEmpty(iqea.InlineQuery.Query) || string.IsNullOrWhiteSpace(iqea.InlineQuery.Query))
+                    return;
+
+                var toloka = new TolokaHurtom.Toloka(iqea.InlineQuery.Query).ToArray();
+
+                if (toloka.Length > 0)
+                {
+                    var inline = new InlineQueryResultArticle[toloka.Length];
+
+                    for (int i = 0; i < toloka.Length; i++)
+                    {
+                        var content = new InputTextMessageContent($"<b>{toloka[i].title}</b>\n{toloka[i].size} | Роздають: {toloka[i].seeders} | Завантажують: {toloka[i].leechers}\n<i>{toloka[i].forum_parent} / {toloka[i].forum_parent}</i>\n{toloka[i].link}");
+                        content.ParseMode = ParseMode.Html;
+
+                        inline[i] = new InlineQueryResultArticle(
+                            i.ToString(),
+                            toloka[i].title,
+                            content);
+
+                        inline[i].Description =
+                            toloka[i].size + " | Роздають: " + toloka[i].seeders + " | Завантажують: " + toloka[i].leechers +
+                            "\n" + toloka[i].forum_name + " / " + toloka[i].forum_parent;
+
+                        inline[i].ThumbUrl = toloka[i].link;
+                    }
+
+                    await Bot.AnswerInlineQueryAsync(iqea.InlineQuery.Id, inline);
+                }
+            };
+
+            Bot.OnMessage += async (object updobj, MessageEventArgs mea) =>
+            {
+                var message = mea.Message;
+
+                if (mea.Message.Type == MessageType.Text)
+                {
+                    if (message.Text == null)
+                        return;
+
+                    var ChatId = message.Chat.Id;
+
+                    string command = message.Text.ToLower().Replace("@hurtombot", "").Replace("/", "");
+
+                    switch (command)
+                    {
+                        case "start":
+                            await Bot.SendTextMessageAsync(ChatId, "Вітаю! Я @HurtomBot!\nНадішліть мені текстове повідомлення щоб знайти українські торренти.\nНатисніть '/', щоби обрати команду.");
+                            break;
+
+                        case "sendtorrent":
+                            await Bot.SendTextMessageAsync(ChatId, "Натисніть кнопку та оберіть чат до якого хочете надіслати торрент.", replyMarkup: new InlineKeyboardMarkup(new[] { InlineKeyboardButton.WithSwitchInlineQuery("Надіслати") }));
+                            break;
+
+                        default:
+                            foreach (var torrent in new TolokaHurtom.Toloka(command).ToArray())
+                                Bot.SendTextMessageAsync(ChatId, $"<b>{torrent.title}</b>\n{torrent.size} | Роздають: {torrent.seeders} | Завантажують: {torrent.leechers}\n<i>{torrent.forum_parent} / {torrent.forum_parent}</i>\n{torrent.link}", ParseMode.Html);
+                            break;
+                    }
+                }
+            };
+
+            Bot.StartReceiving();
+        }
+    }
+}
--- TolokaHurtom/Toloka.cs
+++ TolokaHurtom/Toloka.cs
@@ -3,80 +3,83 @@ using System.Text;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 
-public class Toloka
+namespace TolokaHurtom
 {
-    private const string TolokaUrl = "https://toloka.to/api.php";
-    private const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.17763";
-
-    private Hurtom[] result;
-
-    public Toloka(string search1)
+    public class Toloka
     {
-        using (var webClient = new WebClient())
+        private const string TolokaUrl = "https://toloka.to/api.php";
+        private const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.17763";
+
+        private Hurtom[] result;
+
+        public Toloka(string search1)
         {
-            webClient.Encoding = Encoding.UTF8;
+            using (var webClient = new WebClient())
+            {
+                webClient.Encoding = Encoding.UTF8;
 
-            webClient.Headers.Add("user-agent", UserAgent);
-            webClient.QueryString.Add("search", Regex.Escape(search1));
-            webClient.UseDefaultCredentials = true;
+                webClient.Headers.Add("user-agent", UserAgent);
+                webClient.QueryString.Add("search", Regex.Escape(search1));
+                webClient.UseDefaultCredentials = true;
 
-            result = JsonConvert.DeserializeObject<Hurtom[]>(webClient.DownloadString(TolokaUrl));
+                result = JsonConvert.DeserializeObject<Hurtom[]>(webClient.DownloadString(TolokaUrl));
+            }
         }
-    }
 
-    public Toloka(string search1, string search2)
-    {
-        using (var webClient = new WebClient())
+        public Toloka(string search1, string search2)
         {
-            webClient.Encoding = Encoding.UTF8;
+            using (var webClient = new WebClient())
+            {
+                webClient.Encoding = Encoding.UTF8;
 
-            webClient.Headers.Add("user-agent", UserAgent);
-            webClient.QueryString.Add("search", Regex.Escape(search1));
-            webClient.QueryString.Add("search2", Regex.Escape(search2));
-            webClient.UseDefaultCredentials = true;
+                webClient.Headers.Add("user-agent", UserAgent);
+                webClient.QueryString.Add("search", Regex.Escape(search1));
+                webClient.QueryString.Add("search2", Regex.Escape(search2));
+                webClient.UseDefaultCredentials = true;
 
-            result = JsonConvert.DeserializeObject<Hurtom[]>(webClient.DownloadString(TolokaUrl));
+                result = JsonConvert.DeserializeObject<Hurtom[]>(webClient.DownloadString(TolokaUrl));
+            }
         }
-    }
 
-    public Toloka(string search1, string search2, string search3)
-    {
-        using (var webClient = new WebClient())
+        public Toloka(string search1, string search2, string search3)
         {
-            webClient.Encoding = Encoding.UTF8;
+            using (var webClient = new WebClient())
+            {
+                webClient.Encoding = Encoding.UTF8;
 
-            webClient.Headers.Add("user-agent", UserAgent);
-            webClient.QueryString.Add("search", Regex.Escape(search1));
-            webClient.QueryString.Add("search2", Regex.Escape(search2));
-            webClient.QueryString.Add("search3", Regex.Escape(search3));
-            webClient.UseDefaultCredentials = true;
+                webClient.Headers.Add("user-agent", UserAgent);
+                webClient.QueryString.Add("search", Regex.Escape(search1));
+                webClient.QueryString.Add("search2", Regex.Escape(search2));
+                webClient.QueryString.Add("search3", Regex.Escape(search3));
+                webClient.UseDefaultCredentials = true;
 
-            result = JsonConvert.DeserializeObject<Hurtom[]>(webClient.DownloadString(TolokaUrl));
+                result = JsonConvert.DeserializeObject<Hurtom[]>(webClient.DownloadString(TolokaUrl));
+            }
         }
-    }
 
-    public Hurtom[] ToArray()
-    {
-        return result;
-    }
+        public Hurtom[] ToArray()
+        {
+            return result;
+        }
 
-    public string ToString(string separator = null)
-    {
-        StringBuilder res = new StringBuilder();
+        public string ToString(string separator = null)
+        {
+            StringBuilder res = new StringBuilder();
 
-        foreach (var r in result)
-            res.Append($"{r}{separator}");
+            foreach (var r in result)
+                res.Append($"{r}{separator}");
 
-        return res.ToString();
-    }
+            return res.ToString();
+        }
 
-    public string[] ToStringArray()
-    {
-        string[] res = new string[result.Length];
+        public string[] ToStringArray()
+        {
+            string[] res = new string[result.Length];
 
-        for (int i = 0; i < result.Length; i++)
-            res[i] = result[i].ToString();
+            for (int i = 0; i < result.Length; i++)
+                res[i] = result[i].ToString();
 
-        return res;
+            return res;
+        }
     }
 }
