2005e627923fa778c5e7571c3a569e6a5afb64f5
Вдовенко Віталій <taybenberg@gmail.com>
2020-02-21 22:42:31 +0200

    Upd

--- HurtomBotRunner/Startup.cs
+++ HurtomBotRunner/Startup.cs
@@ -26,7 +26,7 @@ namespace HurtomBotRunner
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            new HurtomBot.HurtomBot(Configuration.GetValue<string>("TelegramBotApiToken"));
+            new HurtomBot.HurtomBot(Environment.GetEnvironmentVariable("TelegramBotApiToken"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
