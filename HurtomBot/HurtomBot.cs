ee4b1ebd27c4ab13e7369803f66f84bc2fd4cc17
Вдовенко Віталій <taybenberg@gmail.com>
2020-08-03 18:45:47 +0300

    Inline categories swap

--- HurtomBot/HurtomBot.cs
+++ HurtomBot/HurtomBot.cs
@@ -36,7 +36,7 @@ namespace HurtomBot
 
                     for (int i = 0; i < toloka.Length; i++)
                     {
-                        var content = new InputTextMessageContent($"<b>{toloka[i].title}</b>\n{toloka[i].size} | Роздають: {toloka[i].seeders} | Завантажують: {toloka[i].leechers}\n<i>{toloka[i].forum_parent} / {toloka[i].forum_parent}</i>\n{toloka[i].link}");
+                        var content = new InputTextMessageContent($"<b>{toloka[i].title}</b>\n{toloka[i].size} | Роздають: {toloka[i].seeders} | Завантажують: {toloka[i].leechers}\n<i>{toloka[i].forum_parent} / {toloka[i].forum_name}</i>\n{toloka[i].link}");
                         content.ParseMode = ParseMode.Html;
 
                         inline[i] = new InlineQueryResultArticle(
@@ -46,7 +46,7 @@ namespace HurtomBot
 
                         inline[i].Description =
                             toloka[i].size + " | Роздають: " + toloka[i].seeders + " | Завантажують: " + toloka[i].leechers +
-                            "\n" + toloka[i].forum_name + " / " + toloka[i].forum_parent;
+                            "\n" + toloka[i].forum_parent + " / " + toloka[i].forum_name;
 
                         inline[i].ThumbUrl = toloka[i].link;
                     }
@@ -80,7 +80,7 @@ namespace HurtomBot
 
                         default:
                             foreach (var torrent in new TolokaHurtom.Toloka(command).ToArray())
-                                bot.SendTextMessageAsync(ChatId, $"<b>{torrent.title}</b>\n{torrent.size} | Роздають: {torrent.seeders} | Завантажують: {torrent.leechers}\n<i>{torrent.forum_name} / {torrent.forum_parent}</i>\n{torrent.link}", ParseMode.Html);
+                                bot.SendTextMessageAsync(ChatId, $"<b>{torrent.title}</b>\n{torrent.size} | Роздають: {torrent.seeders} | Завантажують: {torrent.leechers}\n<i>{torrent.forum_parent} / {torrent.forum_name}</i>\n{torrent.link}", ParseMode.Html);
                             break;
                     }
                 }
